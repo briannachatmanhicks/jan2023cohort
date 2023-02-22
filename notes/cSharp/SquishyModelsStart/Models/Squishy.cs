fbc286e33db33e0997404e83505bf01d55688d4c
John Doe <johndoe@example.com>
2023-02-22 16:17:52 -0600

    saving

---
+++ notes/cSharp/SquishyModelsStart/Models/Squishy.cs
@@ -0,0 +1,12 @@
+#pragma warning disable CS8618
+
+public class Squishy {
+    public string Name {get; set;}
+    public int Age {get; set;}
+
+    public Squishy(string theName, int theAge){
+        Name= theName;
+        Age= theAge;
+    }
+
+}
